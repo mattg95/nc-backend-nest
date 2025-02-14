cf03450eac703aae9a6549474cb71235ea4e9526
mattg95 <mattgoodman95@gmail.com>
2025-02-14 13:17:40 +0000

    seed topics

--- src/seeding/main.seeder.ts
+++ src/seeding/main.seeder.ts
@@ -3,9 +3,8 @@ import { Article } from '../entities/article.entity';
 import { User } from '../entities/user.entity';
 import { Comment } from '../entities/comment.entity';
 
-import { DataSource, Entity } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
-import { ArticleFactory } from './article.factory';
 
 export class MainSeeder implements Seeder {
   public async run(
@@ -21,8 +20,10 @@ export class MainSeeder implements Seeder {
     console.log('seeding users...');
     const users = await usersFactory.saveMany(10);
 
-    console.log('seeding articles...');
-    console.log('seeding comments...');
+    console.log('seeding topics...');
+    const topics = await Promise.all(
+      new Array(15).fill(null).map(() => topicsFactory.save()),
+    );
 
     for (const user of users) {
       const articlesForUser = await Promise.all(
@@ -31,6 +32,11 @@ export class MainSeeder implements Seeder {
 
       for (const article of articlesForUser) {
         article.author = user;
+
+        const numberOfTopics = Math.floor(Math.random() * 4);
+        const randomTopicIndex = Math.floor(Math.random() * topics.length);
+        const topicsForArticle = topics.slice(randomTopicIndex, numberOfTopics);
+        article.topics = topicsForArticle;
 
         await articlesFactory.save(article);
         const commentsForArticle = await Promise.all(
@@ -44,9 +50,5 @@ export class MainSeeder implements Seeder {
         }
       }
     }
-
-    console.log('seeding topics...');
-
-    await topicsFactory.saveMany(10);
   }
 }
